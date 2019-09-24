8e10be2709ba1b7728f014d719bba3c311310692
NitinMore7 <nitinmore6990@gmail.com>
2019-09-25 02:36:51 +0530

    Added predictions

--- app/src/main/java/spider/app/sportsfete19/API/LoginInterface.java
+++ app/src/main/java/spider/app/sportsfete19/API/LoginInterface.java
@@ -1,12 +1,22 @@
 package spider.app.sportsfete19.API;
 
+import com.google.gson.JsonObject;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import spider.app.sportsfete19.predict;
+import spider.app.sportsfete19.predictresponse;
 
 public interface LoginInterface {
 
     @POST("~praveen/auth_jwt.php")
     Call<LoginResponse> requestLogin(@Body LoginData body);
+
+    @Headers("Content-Type: application/json")
+    @POST("votepredictor")
+    Call<String> votepredictor(@Header("Authorization") String jwt, @Body predict body);
 
 }
